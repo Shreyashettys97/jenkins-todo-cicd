fd6d11ac948a237a7c64c7c7693e34cfeb988768
Shreyashettys97 <145067045+Shreyashettys97@users.noreply.github.com>
2023-11-15 02:10:45 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, everyone!'
+    return 'This project is a Flask web application that is built and deployed using Jenkins CI/CD pipeline with Docker. The project is integrated with Github to enable automatic builds and deployments. The pipeline automatically builds the Docker image, tags it, and pushes it to Dockerhub registry. It also deploys the application to an AWS EC2 instance with Docker installed. The project is designed to demonstrate an efficient and automated software development process using DevOps practices.'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
